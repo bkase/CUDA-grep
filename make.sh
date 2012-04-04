2e7102c1722e0f98f338679ec85a1d349fc6108f
Brandon Kase <bkase@cmu.edu>
2012-04-04 09:51:06 -0400

    Made a makefile

--- make.sh
+++ make.sh
@@ -1,1 +1,12 @@
-gcc -Wall -O3 nfa.c -o a.out
+#!/bin/bash
+
+if [[ $1 == '' ]]
+then
+    gcc -Wall -O3 nfa.c -o a.out
+elif [[ $1 == 'debug' ]]
+then
+    gcc -Wall -g3 nfa.c -o a.out
+elif [[ $1 == 'clean' ]]
+then
+    rm -rf a.out
+fi
