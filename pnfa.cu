bcf6f6743fa3a734901d7f3084a82fc8339cd557
Manish Burman <mburman@andrew.cmu.edu>
2012-04-13 22:55:34 -0400

    Copied code to print device data from assignment 2

---
+++ pnfa.cu
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <math.h>
+#include <stdio.h>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+// taken from 15-418 assignment 2
+void
+printCudaInfo() {
+    
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+    
+    printf("Found %d CUDA devices\n", deviceCount);
+    
+    for (int i=0; i<deviceCount; i++) {
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        printf("Device %d: %s\n", i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n",
+               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+    }
+
+}
