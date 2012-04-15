6cf8dba448d3ee05476622750afa065a7a4d6129
Manish Burman <mburman@andrew.cmu.edu>
2012-04-14 20:53:44 -0400

    Modified tests to include .* separately

--- testbench/tests.sh
+++ testbench/tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-teststrings=("ROMEO" "JULIET" "ROMEO|JULIET" "R+" "R*" "R" "R+R*" "R*R+" "RR+"
+teststrings=("ROMEO" "JULIET" "ROMEO|JULIET" "R+" "R*" "R" "R+R*" "R*R+" "RR+" "RR*" 
 "R+|J+" "(R|J)ULIET" 
 #Test for the . wildcard
-"But .hen" "R..EO" "R..EO|...IET" "R..*" ".*"  );
+"But .hen" "R..EO" "R..EO|...IET" "R..*");
 
 date >> RESULTS;
 echo "" >> RESULTS;
@@ -14,7 +14,6 @@ len=${#teststrings[@]}
 for ((i=0; i<len; i++))
 do
 	testcase=${teststrings[i]};
-	echo $testcase;
 	if diff <(../nfa -f ../romeojuliet.txt $i) <(egrep $i ../romeojuliet.txt) >> RESULTS; then
 		cat /dev/null;
 	else
@@ -22,6 +21,14 @@ do
 		echo "Test Failed $testcase";
 	fi
 done
+
+if diff <(../nfa -f ../romeojuliet.txt '.*') <(egrep '.*' ../romeojuliet.txt) >> RESULTS; then
+		cat /dev/null;
+else
+	pass=0
+	echo "Test Failed $testcase";
+fi
+
 
 if [ $pass -eq "1" ]; then
 		echo "All tests passed" >> RESULTS;
