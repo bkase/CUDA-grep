11367ccca0e41dcb9da75315f48cd65e184f456c
Manish Burman <mburman@andrew.cmu.edu>
2012-04-14 23:55:49 -0400

    Forgot to commit the actual testbench

---
+++ testbench/runtests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+teststrings=(
+"ROMEO" "JULIET"
+#Test +*|
+"ROMEO|JULIET" "R+" "R*" "R" "R+R*" "R*R+" "RR+" "RR*" "R+|J+" "(R|J)ULIET" 
+#Test for the . wildcard
+"But .hen" "R..EO" "R..EO|...IET" "R..*"
+#Test for ranges
+"[a-b]" "[q-s]" "[0-9]"
+#Test for the \ sequence
+"\?" "\|" "\+" "\*" "\." "\.\?\|\+\*" 
+#Random tests
+".*\?" "t*hi.\?"
+);
+
+date >> RESULTS
+echo "" >> RESULTS
+pass=1
+
+len=${#teststrings[@]} 
+
+FILES=./testcases/*
+
+#loop over all test cases
+for file in $FILES
+do
+	for ((i=0; i<len; i++))
+	do
+		testcase=${teststrings[i]}
+		if diff <(../nfa -f $file $i) <(egrep $i $file) >> RESULTS; then
+			cat /dev/null
+		else
+			pass=0
+			echo "Test Failed $testcase"
+		fi
+	done
+done
+
+#special test for .*
+for file in $FILES
+do	
+	if diff <(../nfa -f $file '.*') <(egrep '.*' $file) >> RESULTS; then
+		cat /dev/null
+	else
+		pass=0
+		echo "Test Failed .*"
+	fi
+done
+
+if [ $pass -eq "1" ]; then
+		echo "All tests passed" >> RESULTS
+fi
+	
+echo "------------" >> RESULTS
+
