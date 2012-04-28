f3040199db15bb4194ab761cf1d904bf182759d8
Manish Burman <mburman@andrew.cmu.edu>
2012-04-28 15:00:23 -0400

    Fixed a bug with the test bench. Also added new tests for CUDA.

--- testbench/cudatest.sh
+++ testbench/cudatest.sh
@@ -2,16 +2,7 @@
 
 teststrings=(
 "ROMEO" "JULIET"
-#Test +*|
-"ROMEO|JULIET" "R+" "R*" "R" "R+R*" "R*R+" "RR+" "RR*" "R+|J+" "(R|J)ULIET" 
-#Test for the . wildcard
-"R..EO" "R..EO|...IET" "R..*"
-#Test for ranges
-"[a-b]" "[q-s]" "[0-9]"
-#Test for the \ sequence
-"\?" "\|" "\+" "\*" "\." "\.\?\|\+\*" 
-#Random tests
-".*\?" "t*hi.\?"
+"ROMEO|JULIET" "R." "R.MEO|JULIET"
 );
 
 date >> RESULTS
@@ -28,7 +19,6 @@ do
 	for ((i=0; i<len; i++))
 	do
 		testcase=${teststrings[$i]}
-		#echo $testcase
 		if diff <(../nfa -f $file $testcase) <(egrep $testcase $file) >> RESULTS; then
 			cat /dev/null
 		else
@@ -36,17 +26,6 @@ do
 			echo "Test Failed $testcase"
 		fi
 	done
-done
-
-#special test for .*
-for file in $FILES
-do	
-	if diff <(../nfa -f $file '.*') <(egrep '.*' $file) >> RESULTS; then
-		cat /dev/null
-	else
-		pass=0
-		echo "Test Failed .*"
-	fi
 done
 
 if [ $pass -eq "1" ]; then
